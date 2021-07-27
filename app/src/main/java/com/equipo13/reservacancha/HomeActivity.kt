1f268b9d175cab8ca226173f7f74b536a7f608c3
byverbel <byverbel@gmail.com>
2021-07-27 16:43:42 -0500

    Crear repositorio

---
+++ app/src/main/java/com/equipo13/reservacancha/HomeActivity.kt
@@ -0,0 +1,33 @@
+package com.equipo13.reservacancha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType {
+    BASIC
+}
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+    }
+
+    private fun setup(email: String, provider: String){
+        title = "inicio"
+
+        userTextView.text = email
+
+        buttonLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+}
