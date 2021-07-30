d0f363403bec5cdeeca53fcd8a3a21b5ce59231c
byverbel <byverbel@gmail.com>
2021-07-30 15:26:56 -0500

    Register Activity

---
+++ app/src/main/java/com/equipo13/reservacancha/LoginActivity.kt
@@ -0,0 +1,88 @@
+package com.equipo13.reservacancha
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val context = this
+
+
+        // Get logo from the web
+        val logoView = loginLogo
+        Glide.with(this).load("https://i.dlpng.com/static/png/6603159_preview.png")
+            .into(logoView)
+
+        // Obtain the FirebaseAnalytics instance to test logEvents
+        /*val analytics = Firebase.analytics
+        val bundle = Bundle()
+        bundle.putString("message", "Integración con firebase completa")
+        analytics.logEvent("Initscreen", bundle)*/
+
+        login()
+    }
+
+    private fun login() {
+        title = "Login"
+
+        val email = loginEmail.text
+        val password = loginPassword.text
+
+        loginButton.setOnClickListener{
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                FirebaseAuth.getInstance().
+                    signInWithEmailAndPassword(email.toString(), password.toString()).
+                        addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showHome(it.result?.user?.email ?:"", ProviderType.BASIC)
+                            } else {
+                                showToast("Usuario inválido", Toast.LENGTH_LONG)
+                            }
+                        }
+            } else  {
+                showToast("Ingrese datos", Toast.LENGTH_SHORT)
+            }
+        }
+
+        val time = Toast.LENGTH_SHORT
+        registerActivityButton.setOnClickListener{showRegister()}
+
+    }
+
+    /*private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando el usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }*/
+
+    private fun showToast(message:String, length:Int = Toast.LENGTH_SHORT){
+        Toast.makeText(this, message, length).show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent: Intent = Intent(this, HomeActivity::class.java).apply{
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+    private fun showRegister() {
+        val registerIntent: Intent = Intent(this, RegisterActivity::class.java)
+        startActivity(registerIntent)
+    }
+}
---
+++ app/src/main/java/com/equipo13/reservacancha/RegisterActivity.kt
@@ -0,0 +1,81 @@
+package com.equipo13.reservacancha
+
+import android.content.Intent
+import android.graphics.ColorMatrixColorFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        // Get logo from URL
+        val logoView = registerLogo
+        Glide.with(this)
+            .load("https://lh3.googleusercontent.com/proxy/-zGMMdo5bKj9KhRfLYH-zya9hn2d_keweiNgxjL4La_lewQNKoG3IBS_5fuKqxq-EI4yH9covkcLUtL_UpBWxACU6db9pjF72bWpES7B4CIxoyGFNGIVNuJlhCMKCWDSJbNQds0W8B9ikrZPUDKI3zij9KulN5v6YarhMoxP")
+            .into(logoView)
+
+        val negative = floatArrayOf(-1.0f,     0f,     0f,    0f, 255f, // red
+            0f, -1.0f,     0f,    0f, 255f, // green
+            0f,     0f, -1.0f,    0f, 255f, // blue
+            0f,     0f,     0f, 1.0f,   0f)
+
+        logoView.colorFilter = ColorMatrixColorFilter(negative)
+
+        register()
+    }
+
+    private fun register(){
+        title = "Register"
+
+        val name = registerName.text
+        val email = registerEmail.text
+        val password = registerPassword.text
+        val phone = registerPhoneNumber.text
+
+        registerButton.setOnClickListener{
+            if (email.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty() && phone.isNotEmpty()){
+                FirebaseAuth.getInstance().
+                    createUserWithEmailAndPassword(email.toString(),
+                        password.toString()).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showHome(it.result?.user?.email ?:"", ProviderType.BASIC)
+                            } else {
+                                showToast("Registro Inválido", Toast.LENGTH_LONG)
+                            }
+                        }
+            } else {
+                showToast("Faltan campos por llenar")
+            }
+        }
+    }
+
+    /*private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando el usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }*/
+
+    private fun showToast(message:String, length:Int = Toast.LENGTH_SHORT){
+        Toast.makeText(this, message, length).show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent: Intent = Intent(this, HomeActivity::class.java).apply{
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+}
